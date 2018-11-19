76260a6ebee6ef9b626f910751890ae32cea4e27
Renguang Han <413526182@qq.com>
2018-11-19 13:19:22 +0800

    add author & repo site info

--- lazyload.js
+++ lazyload.js
@@ -1,3 +1,8 @@
+/**
+ * Repo: https://github.com/hanrenguang/lazyload
+ * Author: Renguang Han
+ */
+
 ;(function(global, factory) {
     "use strict";
     if (typeof define === "function" && define.amd) { // AMD
